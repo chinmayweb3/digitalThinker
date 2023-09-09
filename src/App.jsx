d62dd422597c63afcfd2798a07d214fbf0322a89
chinmayweb3 <chinmayweb3@gmail.com>
2023-09-09 20:49:24 +0530

    firstSection made

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Preloader from "./components/preloader/preloader";
 import Canva from "./components/canvaPlane/canva";
 import { ReactLenis } from "@studio-freight/react-lenis";
+import FirstSection from "./components/firstSection/firstSec";
 
 function App() {
   const [preloader, setPreloader] = useState(true);
@@ -10,10 +11,7 @@ function App() {
       <ReactLenis root>
         {preloader && <Preloader preloader={preloader} setPreloader={setPreloader} />}
         <Canva />
-        <section className="full-screen flex-center bg-[#999] text-white text-[72px]">
-          <h2 className="z-[21]">sdklfj lkjjlk</h2>
-          <h2 className="z-[19]">sdklfj lkjjkl lkjjlk</h2>
-        </section>
+        <FirstSection preloader={preloader} setPreloader={setPreloader} />
         <section className="full-screen flex-center bg-amber-300 text-white">lksdfjklsdfjkskkj</section>
       </ReactLenis>
     </>
---
+++ src/components/firstSection/firstSec.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FirstSection = ({ preloader, setPreloader }) => {
+  return (
+    <section className="full-screen flex-center bg-[#999] text-white text-[72px]">
+      <h2 className="z-[21]">sdklfj lkjjlk</h2>
+      <h2 className="z-[19]">sdklfj lkjjkl lkjjlk</h2>
+    </section>
+  );
+};
+
+export default FirstSection;
