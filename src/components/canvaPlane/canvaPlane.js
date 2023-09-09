ad30bbb4edaf2b9bb60e6fc9b3b0cc6a49a5cd08
chinmayweb3 <chinmayweb3@gmail.com>
2023-09-09 19:25:11 +0530

    glb file added

--- src/components/canvaPlane/canvaPlane.js
+++ src/components/canvaPlane/canvaPlane.js
@@ -1,10 +1,11 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 export function canvasPlane() {
   const canvasPlane = document.getElementById("canvas-plane");
 
-  const renderer = new THREE.WebGLRenderer();
+  const renderer = new THREE.WebGLRenderer({ alpha: true });
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   canvasPlane.append(renderer.domElement);
@@ -18,24 +19,51 @@ export function canvasPlane() {
   // const orbitControls = new OrbitControls(camera, renderer.domElement);
   // orbitControls.update();
 
+  const assetLoader = new GLTFLoader();
+
+  assetLoader.load(
+    "../../assets/raptor.glb",
+    function (gltf) {
+      scene.add(gltf.scene);
+    },
+    undefined,
+    function (error) {
+      console.error(error);
+    }
+  );
+  // let mixer;
+  // assetLoader.load(
+  //   raptorGlb.href,
+  //   function (gltf) {
+  //     const model = gltf.scene;
+  //     scene.add(model);
+  //     mixer = new THREE.AnimationMixer(model);
+  //     const clips = gltf.animations;
+
+  //     // Play all animations at the same time
+  //     clips.forEach(function (clip) {
+  //       const action = mixer.clipAction(clip);
+  //       action.play();
+  //     });
+  //   },
+  //   undefined,
+  //   function (error) {
+  //     console.error(error);
+  //   }
+  // );
+
   const boxGeometry = new THREE.BoxGeometry(1, 2);
   // MeshBasicMaterial is a skin like CSS
-  const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, transparent: true });
+  const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 
   // Mesh is something that connects the two independent instance boxGeometry and MeshBasicMaterial
   const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
   // and the add, adds the box mesh to the scene
-  scene.add(boxMesh);
-
-  const planeGeo = new THREE.PlaneGeometry(5, 5);
-  const planeMat = new THREE.MeshBasicMaterial({ Color: 0xffffff, side: 2, transparent: true });
-  const planeMesh = new THREE.Mesh(planeGeo, planeMat);
-  scene.add(planeMesh);
-  planeMesh.rotation.x = -0.5 * Math.PI;
+  // scene.add(boxMesh);
 
   // grid is something same as axishelper
-  const grid = new THREE.GridHelper(5, 5);
-  scene.add(grid);
+  // const grid = new THREE.GridHelper(5, 5);
+  // scene.add(grid);
 
   const animate = (time) => {
     // boxMesh.rotation.z = time / 1000;
