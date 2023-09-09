6077d026ab0e94239de535b025fd6d856d1d53ec
chinmayweb3 <chinmayweb3@gmail.com>
2023-09-09 11:52:56 +0530

    added libraries

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { motion } from "framer-motion";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useEffect, useState } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [remove, setRem] = useState(false);
+  useEffect(() => {
+    if (!remove) return;
+    gsap.to("#animated-hidden", {
+      delay: 1,
+      top: "-100vh",
+      onComplete: () => {
+        document.getElementsByTagName("body")[0].style.overflow = "auto";
+      },
+    });
+  }, [remove]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* <ReactLenis root option={{ smoothWheel: true, infinite: true }}> */}
+      <div id="animated-hidden" className="fixed top-0 left-0 bg-lime-500 h-[100vh] w-full flex justify-center items-center overflow-">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.8 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 2 }}
+          className="font-mono flex flex-col justify-start items-center h-[350px] "
+        >
+          <span className="font-thin text-[32px]">This is it </span>
+          <h2 className=" text-[72px] font-black ">HELLO WORLD</h2>
+        </motion.div>
+        <motion.div
+          onAnimationComplete={() => setRem(true)}
+          animate={{
+            width: "100vw",
+            height: "100vh",
+            top: "0",
+            bottom: "auto",
+            transform: "translateY(0)",
+            borderRadius: 0,
+          }}
+          transition={{ duration: 4, ease: "easeInOut" }}
+          className="absolute bottom-0 translate-y-[50%] bg-slate-300 w-[100px] h-[100px] rounded-tl-full rounded-tr-full "
+        ></motion.div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <section className="bg-slate-600 min-h-screen w-full flex justify-center items-center">lksdfjklsdfjkskkj</section>
+      <section className="bg-red-600 min-h-screen w-full flex justify-center items-center">lksdfjklsdfjkskkj</section>
+      <section className="bg-gray-600 min-h-screen w-full flex justify-center items-center">lksdfjklsdfjkskkj</section>
+      {/* </ReactLenis> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
