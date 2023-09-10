c48ab85c4299546c9e8e5482039e188eefd162f0
chinmayweb3 <chinmayweb3@gmail.com>
2023-09-10 15:36:22 +0530

    usecanvasPlane

--- src/components/canvaPlane/canva.jsx
+++ src/components/canvaPlane/canva.jsx
@@ -1,11 +1,9 @@
 import React, { useEffect } from "react";
 import "./canva.scss";
-import "./canvaPlane.js";
-import { canvasPlane } from "./canvaPlane.js";
+import { useCanvasPlane } from "./canvaPlane.js";
 const Canva = ({ preloader, setPreloader }) => {
-  useEffect(() => {
-    canvasPlane(preloader);
-  }, [preloader]);
+  useCanvasPlane(preloader);
+
   return <section id="canvas-plane" className="fixed z-[20] full-screen"></section>;
 };
 
--- src/components/canvaPlane/canvaPlane.js
+++ src/components/canvaPlane/canvaPlane.js
@@ -3,6 +3,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { GUI } from "dat.gui";
 import { Back, Elastic, gsap } from "gsap";
+import { useEffect } from "react";
 
 const sizes = {
   width: window.innerWidth,
@@ -37,108 +38,110 @@ const movement = [
   },
 ];
 
-export async function canvasPlane(preloader) {
-  const canvasPlane = document.getElementById("canvas-plane");
+export const useCanvasPlane = (preloader) => {
+  useEffect(() => {
+    const canvasPlane = document.getElementById("canvas-plane");
 
-  const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
-  renderer.setSize(window.innerWidth, window.innerHeight);
+    const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+    renderer.setSize(window.innerWidth, window.innerHeight);
 
-  canvasPlane.append(renderer.domElement);
+    canvasPlane.append(renderer.domElement);
 
-  const scene = new THREE.Scene();
+    const scene = new THREE.Scene();
 
-  const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  camera.position.set(0, 0, 5);
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    camera.position.set(0, 0, 5);
 
-  // const orbitControls = new OrbitControls(camera, renderer.domElement);
-  // orbitControls.update();
+    // const orbitControls = new OrbitControls(camera, renderer.domElement);
+    // orbitControls.update();
 
-  const ambientLight = new THREE.AmbientLight(0xffffff, Math.PI);
-  scene.add(ambientLight);
+    const ambientLight = new THREE.AmbientLight(0xffffff, Math.PI);
+    scene.add(ambientLight);
 
-  // a helper to identify  the center of the scene
-  const axes = new THREE.AxesHelper(10);
-  // add the axes to the scene
-  scene.add(axes);
+    // a helper to identify  the center of the scene
+    const axes = new THREE.AxesHelper(10);
+    // add the axes to the scene
+    scene.add(axes);
 
-  let model3d;
-  const assetLoader = new GLTFLoader();
-  assetLoader.load(
-    "./sukhoi.glb",
-    (glb) => {
-      model3d = glb.scene;
+    let model3d;
+    const assetLoader = new GLTFLoader();
+    assetLoader.load(
+      "./sukhoi.glb",
+      (glb) => {
+        model3d = glb.scene;
 
-      model3d.scale.set(0.1, 0.1, 0.1);
-      model3d.position.x = Math.PI * 3;
-      model3d.position.x = Math.PI * 3;
-      // model3d.rotation.x = Math.PI * 1;
-      model3d.rotation.y = -1;
-      // model3d.rotation.z = -1;
-      // model3d.rotation.x = 1;
+        model3d.scale.set(0.1, 0.1, 0.1);
+        model3d.position.x = Math.PI * 3;
+        model3d.position.x = Math.PI * 3;
+        // model3d.rotation.x = Math.PI * 1;
+        model3d.rotation.y = -1;
+        // model3d.rotation.z = -1;
+        // model3d.rotation.x = 1;
 
-      scene.add(model3d);
-      model3dGui(model3d);
-    },
-    (xhr) => {
-      // console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
-    },
-    // called when loading has errors
-    (err) => {
-      console.log(" \n\n\n\nAn error happened", err);
-    }
-  );
+        scene.add(model3d);
+        model3dGui(model3d);
+      },
+      (xhr) => {
+        // console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+      },
+      // called when loading has errors
+      (err) => {
+        console.log(" \n\n\n\nAn error happened", err);
+      }
+    );
 
-  // scroll animation started
-  // scroll animation started
-  let scrollY = window.scrollY;
-  let currentSection = 0;
-  if (preloader && model3d) {
-    gsap.to(model3d.rotation, {
-      duration: 1,
-      // delay: 4,
-      x: movement[newSection].rotationX,
-      y: movement[newSection].rotationY,
-      z: movement[newSection].rotationZ,
-      ease: Back.easeInOut(1),
-    });
-  }
-
-  window.addEventListener("scroll", () => {
-    scrollY = window.scrollY;
-    const newSection = Math.round(scrollY / window.innerHeight);
-    console.log("New section scrollY: " + newSection, currentSection);
-
-    // if (currentSection !== newSection) {
-    if (!!model3d) {
+    // scroll animation started
+    // scroll animation started
+    let scrollY = window.scrollY;
+    let currentSection = 0;
+    if (preloader && model3d) {
       gsap.to(model3d.rotation, {
         duration: 1,
+        // delay: 4,
         x: movement[newSection].rotationX,
         y: movement[newSection].rotationY,
         z: movement[newSection].rotationZ,
         ease: Back.easeInOut(1),
       });
-      gsap.to(model3d.position, {
-        duration: 1,
-        x: movement[newSection].positionX,
-        y: movement[newSection].positionY,
-        z: movement[newSection].positionZ,
-      });
     }
-    // }
-  });
 
-  const animate = (time) => {
-    // renderer .render runs the scene on to the getmainId which will render it to the screen
-    // boxMesh.rotation.y = Math.sin(time / 1000);
-    if (model3d) {
-      model3d.position.y = Math.sin(time / 1000) * 0.1;
-      model3d.rotation.z = Math.sin(time / 1000) * 0.08;
-    }
-    renderer.render(scene, camera);
-  };
+    window.addEventListener("scroll", () => {
+      scrollY = window.scrollY;
+      const newSection = Math.round(scrollY / window.innerHeight);
+      console.log("New section scrollY: " + newSection, currentSection);
 
-  renderer.setAnimationLoop(animate);
-}
+      // if (currentSection !== newSection) {
+      if (!!model3d) {
+        gsap.to(model3d.rotation, {
+          duration: 1,
+          x: movement[newSection].rotationX,
+          y: movement[newSection].rotationY,
+          z: movement[newSection].rotationZ,
+          ease: Back.easeInOut(1),
+        });
+        gsap.to(model3d.position, {
+          duration: 1,
+          x: movement[newSection].positionX,
+          y: movement[newSection].positionY,
+          z: movement[newSection].positionZ,
+        });
+      }
+      // }
+    });
+
+    const animate = (time) => {
+      // renderer .render runs the scene on to the getmainId which will render it to the screen
+      // boxMesh.rotation.y = Math.sin(time / 1000);
+      if (model3d) {
+        model3d.position.y = Math.sin(time / 1000) * 0.1;
+        model3d.rotation.z = Math.sin(time / 1000) * 0.08;
+      }
+      renderer.render(scene, camera);
+    };
+
+    renderer.setAnimationLoop(animate);
+  }, []);
+};
 
 const model3dGui = (model3d) => {
   const gui = new GUI();
